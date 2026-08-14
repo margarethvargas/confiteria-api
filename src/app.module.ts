38d4b0a01788b20e3187ac7f135ac6f3df43a986
Margareth Vargas <margarethvargass@gmail.com>
2026-08-14 12:30:47 -0500

    fix database password environment variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,7 +20,7 @@ import { CategoriesModule } from './category/category.module';
         host: config.get<string>('DB_HOST'),
         port: config.get<number>('DB_PORT'),
         username: config.get<string>('DB_USER'),
-        password: config.get<string>('DB_PASS'),
+        password: config.get<string>('DB_PASSWORD'),
         database: config.get<string>('DB_NAME'),
 
         autoLoadEntities: true,
